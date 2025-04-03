ddc1ab207f14b9ce0147947797196505d6f5d12a
pratiktawhare <pratiktawhare3@gmail.com>
2025-04-04 02:43:04 +0530

    new commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const wrapAsync = require("../utils/wrapAsync.js");
+const wrapAsync = require("./utils/wrapAsync.js");
 const User = require("./models/user");
 
 const Listing = require("./models/listing.js");
