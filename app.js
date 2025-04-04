6a8cb58edb470bee7af92e4231fbbba90ddff450
pratiktawhare <pratiktawhare3@gmail.com>
2025-04-04 17:21:18 +0530

    updated / route

--- app.js
+++ app.js
@@ -84,14 +84,14 @@ app.use((req, res, next) => {
 })
 
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-app.get("/", wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
-}))
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page not found!"));
